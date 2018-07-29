c8c835caaec7515dad494ed76c3e6861abdff5e3
Gamcas98 <40047389+Gamcas98@users.noreply.github.com>
2018-07-29 10:53:06 -0600

    Update Practica1.java

--- Practica 1/src/practica/pkg1/Practica1.java
+++ Practica 1/src/practica/pkg1/Practica1.java
@@ -15,6 +15,7 @@ public class Practica1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("puto el que lo lea");
         // TODO code application logic here
     }
     
